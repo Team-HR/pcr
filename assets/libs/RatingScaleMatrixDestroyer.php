136f22a491733f8e77b6fe99f611a0436616fbe0
fjavalencia <franzval123@gmail.com>
2023-10-17 13:22:39 +0800

    feat: RSM DELETER using period_id and department_id

---
+++ assets/libs/RatingScaleMatrixDestroyer.php
@@ -0,0 +1,120 @@
+<?php
+
+class RatingScaleMatrixDestroyer
+{
+	private $mysqli;
+	private $period_id;
+	private $department_id;
+	private $mfos;
+
+	public function __construct($mysqli)
+	{
+		$this->mysqli = $mysqli;
+	}
+
+
+	public function set_period_id($period_id)
+	{
+		$this->period_id = $period_id;
+	}
+
+	public function set_department_id($department_id)
+	{
+		$this->department_id = $department_id;
+	}
+
+	public function get_rating_scale_matrix()
+	{
+		$data = [];
+		$data = $this->get_mfos();
+
+		foreach ($data as $mfo) {
+			# delete mfo
+			$cf_ID = $mfo['cf_ID'];
+			// call function destroy_mfo($cf_ID)
+			$sql = "DELETE FROM `spms_corefunctions` WHERE `cf_ID` = '$cf_ID'";
+			$this->mysqli->query($sql);
+			# delete success indicators
+			// call function destroy_si($mi_id)
+			foreach ($mfo['success_indicators'] as $success_indicator) {
+				$mi_id = $success_indicator['mi_id'];
+				$sql = "DELETE FROM `spms_matrixindicators` WHERE `mi_id` = '$mi_id'";
+				$this->mysqli->query($sql);
+				# delete mfo data
+				// call function destroy_cfd($cfd_id)
+				foreach ($success_indicator['cfd_ids'] as $cfd_id) {
+					$sql = "DELETE FROM `spms_corefucndata` WHERE `cfd_id` = '$cfd_id'";
+					$this->mysqli->query($sql);
+				}
+			}
+		}
+
+		return $data;
+	}
+
+
+	private function get_mfos()
+	{
+		$period_id = $this->period_id;
+		$department_id = $this->department_id;
+		$data = [];
+		# first get all mfos with no parent (this will be the top-most parent of the rsm, where department_id and period_id)
+		$sql = "SELECT * from spms_corefunctions where parent_id='' and mfo_periodId='$period_id' and dep_id='$department_id' ORDER BY `spms_corefunctions`.`cf_count` ASC ";
+		$res = $this->mysqli->query($sql);
+		while ($row = $res->fetch_assoc()) {
+			$id = $row['cf_ID'];
+			// $row["children"] = $this->get_children($data, $id);
+			$row["success_indicators"] = $this->get_success_indicators($id);
+			$data[] = [
+				"cf_ID" => $row["cf_ID"],
+				"success_indicators" => $row["success_indicators"]
+			];
+			$this->get_children($data, $id);
+		}
+
+		return $data;
+	}
+
+	private function get_children(&$data, $parent_id)
+	{
+		$sql = "SELECT * from spms_corefunctions where parent_id='$parent_id' ORDER BY `spms_corefunctions`.`cf_count` ASC ";
+		$res = $this->mysqli->query($sql);
+		while ($row = $res->fetch_assoc()) {
+			$id = $row['cf_ID'];
+			$row["success_indicators"] = $this->get_success_indicators($id);
+			$data[] = [
+				"cf_ID" => $row["cf_ID"],
+				"success_indicators" => $row["success_indicators"]
+			];
+			$this->get_children($data, $id);
+		}
+		return $data;
+	}
+	private function get_success_indicators($parent_id)
+	{
+		$data = [];
+		$sql = "SELECT * from spms_matrixindicators where cf_ID='$parent_id'";
+		$res = $this->mysqli->query($sql);
+		while ($row = $res->fetch_assoc()) {
+
+			$data[] = [
+				"mi_id" => $row["mi_id"],
+				"cfd_ids" => $this->get_spms_corefucndata($row["mi_id"])
+			];
+		}
+		return $data;
+	}
+
+	private function get_spms_corefucndata($p_id)
+	{
+		$data = [];
+		$sql = "SELECT * FROM `spms_corefucndata` WHERE `p_id` = '$p_id'";
+		$res = $this->mysqli->query($sql);
+
+		while ($row = $res->fetch_assoc()) {
+			$data[] = $row['cfd_id'];
+		}
+
+		return $data;
+	}
+}
--- test.php
+++ test.php
@@ -1,75 +1,25 @@
 <?php
-require_once "assets/libs/FinalNumericalRatings.php";
+
+require_once "assets/libs/RatingScaleMatrixDestroyer.php";
 
 date_default_timezone_set("Asia/Manila");
 $host = "localhost";
 $usernameDb = "admin";
-// $password = "teamhrmo2019";
 $password = "teamhrmo2019";
 $database = "ihris";
 $mysqli = new mysqli($host, $usernameDb, $password, $database);
 $mysqli->set_charset("utf8");
 #####################################################################################
 
-$period_id = 10; //10 - July to Dec 2022
-$finalNumericalRating = new FinalNumericalRating();
 
-# performanceReviewStatus_id = 2434 test fomtype 3 strategic function shoul be excluded from computing final numerical rating
-$sql = "SELECT * FROM `spms_performancereviewstatus` where `period_id` = '$period_id' AND `department_id` = 32 limit 2";
-//--`performanceReviewStatus_id` = '2909'
-//--`period_id` = '$period_id' AND `final_numerical_rating` IS NULL LIMIT 1
-// $res = $mysqli->query($sql);
-// $data = [];
-// while ($row = $res->fetch_assoc()) {
-//     // $row['final_numerical_rating'] = $finalNumericalRating->getFinalNumericalRating($mysqli, $row);
-//     $final_numerical_rating = $finalNumericalRating->getFinalNumericalRating($mysqli, $row);
-//     $row['final_numerical_rating'] = $final_numerical_rating;
-//     $fileStatusId = $row['performanceReviewStatus_id'];
-//     $finalNumericalRating->setFinalNumericalRating($mysqli, $fileStatusId, $final_numerical_rating);
-//     // setFinalNumericalRating
-//     $data[] = $row;
-// }
-
-print("<pre>" . print_r($data, true) . "</pre>");
+$rsm = new RatingScaleMatrixDestroyer($mysqli);
 
 
-/*
-$fileStatus = [
-    'performanceReviewStatus_id' => '',
-    'period_id' => 11,
-    'employees_id' => 9,
-    'ImmediateSup' => '',
-    'DepartmentHead' => '',
-    'HeadAgency' => '',
-    'PMT' => '',
-    'submitted' => '',
-    'certify' => '',
-    'approved' => '',
-    'panelApproved' => '',
-    'dateAccomplished' => '',
-    'formType' => '1',
-    'department_id' => '32',
-    'assembleAll' => '',
-];
+$rsm->set_period_id(12);
+$rsm->set_department_id(16);
+$data = $rsm->get_rating_scale_matrix();
+
+$json = json_encode($data, JSON_PRETTY_PRINT);
 
 
-$strategic = strategicTr($mysqli, $fileStatus);
-$core = coreRow($mysqli, $fileStatus);
-$support = supportFunctionTr($mysqli, $fileStatus);
-
-$final_numerical_rating = '';
-if ($strategic > 0 && $core > 0 && $support > 0) {
-    $final_numerical_rating = $strategic + $core + $support;
-}
-
-
-print "strategic => " . $strategic;
-print "<br/>";
-print "core =>  " . $core;
-print "<br/>";
-print "support => " . $support;
-print "<br/>";
-print "final => " . $final_numerical_rating;
-
-
-*/
+echo "<pre>$json</pre>";
