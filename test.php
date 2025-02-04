922901b47c3e4b43d8777904e0ffc28f6e4ce59d
fjavalencia <franzval123@gmail.com>
2025-02-04 12:51:35 +0800

    added: year 2025 option in PMT page period selection

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 require_once "assets/libs/RatingScaleMatrixDestroyer.php";
 
 date_default_timezone_set("Asia/Manila");
-$host = "localhost";
+$host = "db";
 $usernameDb = "admin";
 $password = "teamhrmo2019";
 $database = "ihris";
@@ -13,8 +13,8 @@ $mysqli->set_charset("utf8");
 
 $rsm = new RatingScaleMatrixDestroyer($mysqli);
 
-$rsm->set_period_id(12);
-$rsm->set_department_id(1);
+$rsm->set_period_id(22);
+$rsm->set_department_id(10);
 // $data = $rsm->delete_rating_scale_matrix();
 
 $json = json_encode($data, JSON_PRETTY_PRINT);
