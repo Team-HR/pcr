afa8aba945af91a561ecb1bf4c97f8b1ebb971dc
FranzValencia <franzval123@gmail.com>
2026-06-15 14:20:03 +0800

    feat: update nav link to RSM Structure

--- assets/pages/header.php
+++ assets/pages/header.php
@@ -93,7 +93,7 @@
 			} else if (strtoupper($user->authorization[$index]) == strtoupper("Matrix")) {
 			?>
 				<a class="item" href="?MotherRatingScale">Rating Scale Matrix</a>
-				<a class="item" href="?MotherRatingScale&Tree"><i class="icon sitemap"></i> RSM Map</a>
+				<a class="item" href="?MotherRatingScale&Tree"><i class="icon sitemap"></i> RSM Structure</a>
 			<?php
 			} else if (strtoupper($user->authorization[$index]) == strtoupper('pmt')) {
 			?>
