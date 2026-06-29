668b7d607b4e902cdd3236f3a345d81970a096bb
FranzValencia <franzval123@gmail.com>
2026-06-29 10:29:21 +0800

    feat: add Vue 3 production build file to assets
    - Add vue.global.prod.js (v3.5.14) to assets/libs/vue directory
    - Minified production build with MIT license header

--- assets/pages/header.php
+++ assets/pages/header.php
@@ -11,7 +11,7 @@
 	<!-- Chart.js v4.4.0 -->
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 	<!-- Addition of Vue3js -->
-	<script src="https://cdn.jsdelivr.net/npm/vue@3.4.21/dist/vue.global.min.js"></script>
+	<script src="assets/libs/vue/vue.global.prod.js"></script>
 
 	<script src="assets/libs/umbra.js?v=<?= time() ?>"></script>
 	<style media="screen">
